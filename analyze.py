d1a1209e84949d7fc676cb1023cdb47fa7fdbfa7
calren <calren24@gmail.com>
2015-06-27 23:45:10 -0700

    Trying to take out words

--- analyze.py
+++ analyze.py
@@ -4,13 +4,14 @@ import sys
 from urllib import urlopen
 import yaml
 import csv
+from collections import OrderedDict
 
 # there's a common_english_words.txt file that lists the most common ~300 english words, we ignore those in our list
 file = open("common_english_words.txt", 'r')
 common_words = file.read().lower()
 file.close()
-text = re.sub('[^a-z\ \']+', " ", common_words)
 common_words_list = list(common_words.split())
+print common_words_list.decode('string_escape')
 
 # get all books and get url for text
 with open('/Users/carenchang/Desktop/repos_list.tsv') as tsvfile:
@@ -25,9 +26,10 @@ with open('/Users/carenchang/Desktop/repos_list.tsv') as tsvfile:
         file.close()
         # take out anything that's not a letter
         text = re.sub('[^a-z\ \']+', " ", text)
+        text = re.sub(' [^a-z]+', "", text)
         # split the words by spaces
         words = list(text.split())
-
+        
         # for each words in the list of words, if it has already been seen increase its count, otherwise add it to the list
         uniqueWords = dict();
         for word in words:
@@ -36,14 +38,19 @@ with open('/Users/carenchang/Desktop/repos_list.tsv') as tsvfile:
                 uniqueWords[key] += 1
             elif key not in common_words_list:
                 uniqueWords[key] = 1
-
+    
         # sort the list of unique words by most seen
         sorted_unique_words = sorted(uniqueWords.items(), key=operator.itemgetter(1), reverse=True)
-
+        sorted_unique_words_dict = OrderedDict((sorted_unique_words)[:199])
+        
         # get file to save name
-        book_yml_file = book_name + ".yml"
+        book_yml_file = book_name + ".csv"
+        
+        with open(book_yml_file, 'wb') as f:    #w for python 2.7
+            fieldnames = ['word', 'appearances']
+            w = csv.DictWriter(f, fieldnames=fieldnames)
+            w.writeheader()
+            for k, v in sorted_unique_words_dict.items():
+                w.writerow({'word' : k, 'appearances': v})
+# print( "total number of unique words: " + str(len(uniqueWords)));
 
-        with open(book_yml_file, 'w') as outfile:
-            outfile.write( yaml.dump(sorted_unique_words[:49], default_flow_style=False) )
-
-        # print( "total number of unique words: " + str(len(uniqueWords)));
