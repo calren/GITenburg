1c764d73ff13afaf297c770aa7241c0d1d81416e
calren <calren24@gmail.com>
2015-06-27 21:37:26 -0700

    Adding comments for script

--- analyze.py
+++ analyze.py
@@ -3,22 +3,28 @@ import operator
 import sys
 from urllib import urlopen
 
+# book_file should be the raw text file from github, such as
+# https://raw.githubusercontent.com/GITenberg/Les-Mis-rables_135/master/135.txt
 book_file = sys.argv[1]
 
+# there's a common_english_words.txt file that lists the most common ~300 english words, we ignore those in our list
 file = open("common_english_words.txt", 'r')
 common_words = file.read().lower()
 file.close()
 text = re.sub('[^a-z\ \']+', " ", common_words)
 common_words_list = list(common_words.split())
 
-
+# get the file
 file = urlopen(book_file, 'r')
+# save the file in a list with all lower cased letters
 text = file.read().lower()
 file.close()
+# take out anything that's not a letter
 text = re.sub('[^a-z\ \']+', " ", text)
+# split the words by spaces
 words = list(text.split())
-print len(words);
 
+# for each words in the list of words, if it has already been seen increase its count, otherwise add it to the list
 uniqueWords = dict();
 for word in words:
     key = word
@@ -27,9 +33,14 @@ for word in words:
     elif key not in common_words_list:
         uniqueWords[key] = 1
 
+# sort the list of unique words by most seen
 sorted_unique_words = sorted(uniqueWords.items(), key=operator.itemgetter(1), reverse=True)
-sorted_unique_words_dict = dict(sorted(uniqueWords.items(), key=operator.itemgetter(1), reverse=True))
+# sorted_unique_words_dict = dict(sorted(uniqueWords.items(), key=operator.itemgetter(1), reverse=True))
 
-for x in range(1, 50):
-    print (sorted_unique_words[x]);
-# print(len(uniqueWords));
+# print the most seen 50 words
+for k,v in sorted_unique_words[:49]:
+    print k, v
+#print str(k) + " appears " + str(v) + " times"
+
+# print the number of total unique words
+print( "total number of unique words: " + str(len(uniqueWords)));
